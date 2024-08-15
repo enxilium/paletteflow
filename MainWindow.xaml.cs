adca1c1c49dab375191f3b7da9c6e7904d56f9ff
Jace <enxilium@gmail.com>
2024-08-15 10:56:26 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace paletteflow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Go to home screen
+        }
+
+        private void EnterButton_DragEnter(object sender, DragEventArgs e)
+        {
+            EnterButton.FontSize = 13;
+        }
+
+        private void EnterButton_DragLeave(object sender, DragEventArgs e)
+        {
+            EnterButton.FontSize = 12;  
+        }
+    }
+}
